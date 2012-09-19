cedb36311c9e6b8f45d0646ad2731dd4c201f6dd
Chris Simpson <simmousm@gmail.com>
2012-09-19 20:43:16 +0100

    Correct assembly name spelling

--- CherryTomato/Properties/AssemblyInfo.cs
+++ CherryTomato/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CherryTomatoe")]
+[assembly: AssemblyTitle("CherryTomato")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("CherryTomatoe")]
+[assembly: AssemblyProduct("CherryTomato")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
